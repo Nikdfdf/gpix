67e6866df2d15d265eea6e39c8cb0aaa78b484e3
Nikdfdf <mokrinnik1@gmail.com>
2020-07-11 05:23:15 +0300

    mobail

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
-function search() {
-    const inputSearch = document.querySelector('.form-search__input')
-    const imgSearch = document.querySelector('.search__img')
-    imgSearch.addEventListener('click', IsSeacrch);
-    const body = document.querySelector('body')
-    body.addEventListener('click', hid)
-    function IsSeacrch() {
-        if (inputSearch.classList.contains('unactive')) {
-            inputSearch.classList.toggle('unactive');
-            body.style.background = '#737272';
-            inputSearch.focus()
-        } else {
-            inputSearch.classList.toggle('unactive');
-            body.style.background = '#fff'
-        }
+const img = document.querySelector('.search__img'),
+    modal = document.querySelector('.modal'),
+    modalItem = document.querySelector('.modal-item'),
+    itemInput = document.querySelector('.item__input');
+
+img.addEventListener('click', () => {
+    if (img.classList.contains('active')) {
+        img.classList.remove('active');
+        modal.classList.remove('active');
     }
-    function hid(e) {
-        if(e.target != inputSearch && e.target != imgSearch){
-            inputSearch.classList.toggle('unactive');
-            body.style.background = '#fff'
-        }
+    else {
+        img.classList.add('active');
+        modal.classList.add('active');
     }
-}
-search()
+});
+
+modal.addEventListener('click', function(e) {    
+    if(e.target !== modal ) {
+        this.classList.add('active');
+    }
+    else {
+        this.classList.remove('active');
+        img.classList.remove('active');
+    }    
+});
+
+
+const menuBtn = document.querySelector('.header-mobail'),
+    show = document.querySelector('.header-menu');
+
+menuBtn.addEventListener('click', function() {
+    if(this.classList.contains('active')) {
+        this.classList.remove('active');
+        show.classList.remove('active');
+    }
+    else {
+        this.classList.add('active');
+        show.classList.add('active');
+    }
+});
+
+show.addEventListener('click', function(e) {
+    if(e.target == show ) {
+        menuBtn.classList.add('active');
+        show.classList.add('active');
+    }
+    else {
+        menuBtn.classList.remove('active');
+        show.classList.remove('active');
+    }
+});
+
+
+const changeModal = document.querySelector('.header-modal');
+setTimeout(() => {
+    changeModal.classList.add('active');
+}, 3000)
+
+
+const body = document.querySelector('body');
+body.addEventListener('click', function(e) {
+    if (e.taget !== changeModal) {
+        changeModal.classList.remove('active');
+    }
+});
